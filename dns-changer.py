cf0c8a51ad2d72a20d9aa48962ee097ca44f70e4
mart974 <mohammad.bajalal546@gmail.com>
2021-06-03 16:41:08 +0430

    updated

--- dns-changer.py
+++ dns-changer.py
@@ -3,20 +3,27 @@ from termcolor import colored
 from pyfiglet import figlet_format
 print(colored(figlet_format("dns changer", justify="center"), "green"))
 
-ask = input("set dns or back to default: (d/b) \n")
 dns_list = {
     'google' : '8.8.8.8',
     'cisco' :'4.2.2.4',
     'shakan' : '185.51.200.2',
 }
+for name in dns_list :
+    print(f"- {name} \n ")
 
-print("dns : \n1) google \n2) cisco \n3) shekan")
+ask = input("set dns or back to default: (s/b) \n")
 
-if ask == 'd':
+if ask[0] == 's':
+    print("for set dns in list type ur dns by name")
     dns = input("type ur dns : \n")
     if '.' in dns:
-        system(f'echo nameserver  {dns} | sudo tee -a /etc/resolv.conf')
+        system(f'echo nameserver  {dns} | sudo tee -a /etc/resolv.conf >> /dev/null')
+        print(f"dns {dns} added")
+    elif dns in dns_list:
+        system(f'echo nameserver  {dns_list[dns]} | sudo tee -a /etc/resolv.conf >> /dev/null')
+        print(f"dns {dns} added")
     else :
         print("invalid dns")
-elif ask == 'b':
+elif ask[0] == 'b':
+    print("restarting ...")
     system("sudo systemctl restart NetworkManager")
