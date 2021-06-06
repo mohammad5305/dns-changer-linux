67154cd4b0670984ed7a45033d551b60d87b986d
mart974 <mohammad.bajalal546@gmail.com>
2021-06-06 18:31:10 +0430

    just commend added

--- dns-changer.py
+++ dns-changer.py
@@ -12,7 +12,7 @@ dns_list = {
 for name in dns_list:
     print(f"- {name} \n ")
 
-ask = input("set dns or back to default: (s/b) \n")
+ask = input("set dns or set just one dns (recommended for shekan) or back to default: (s/j/b) \n")
 
 if ask[0] == 's':
     print("for set dns in list type ur dns by name")
@@ -24,7 +24,19 @@ if ask[0] == 's':
         system(f'echo nameserver  {dns_list[dns]} | sudo tee -a /etc/resolv.conf >> /dev/null')
         print(f"dns {dns} added")
     else:
-        print("invalid dns")
+        print("Invalid dns")
+elif ask[0] == 'j':
+    print("for set dns in list type ur dns by name")
+    dns = input("type ur dns : \n")
+    if '.' in dns:
+        system(f'echo nameserver  {dns} | sudo tee /etc/resolv.conf >> /dev/null')
+        print(f"dns changed to {dns}")
+    elif dns in dns_list:
+        system(f'echo nameserver  {dns_list[dns]} | sudo tee /etc/resolv.conf >> /dev/null')
+        print(f"dns changed to {dns}")
+    else:
+        print("Invalid dns")
+
 elif ask[0] == 'b':
     print("restarting ...")
     system("sudo systemctl restart NetworkManager")
